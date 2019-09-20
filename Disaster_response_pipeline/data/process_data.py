315f8210e73e2bea195931f64e05e2a597886a9b
LucasPontes <lucas.pontesm@gmail.com>
2019-09-20 16:57:18 -0300

    process_data and model train start working

--- Disaster_response_pipeline/data/process_data.py
+++ Disaster_response_pipeline/data/process_data.py
@@ -1,16 +1,71 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    - Takes two CSV files as input
+    - Imports them as pandas dataframe
+    - Merges it into a dataframe
+    Args:
+    messages_file_path str: Messages CSV file
+    categories_file_path str: Categories CSV file
+    Returns:
+    pandas df with messages and categories files
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return pd.merge(messages, categories)
 
 
 def clean_data(df):
-    pass
+    """
+    - Cleans the combined dataframe    
+    Args:
+    df: Merged pandas dataframe from load_data() function
+    Returns:
+    df: Cleaned data
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(";", expand = True)
+    # extract a list of new column names for categories.
+    row = categories.iloc[0,:]    
+    category_colnames = row.apply(lambda x: x[:-2])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    # Convert category values to numbers 0 or 1    
+    for column in categories:    
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # drop the original categories column from `df`
+    df.drop('categories', axis = 1, inplace = True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = df.merge(categories, right_index=True, left_index=True)
+    # drop duplicates
+    df.drop_duplicates(inplace = True)
+    df.drop('related', axis =1, inplace = True)
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Saves data to a SQL database
+
+    Args:
+    df: Cleaned df from clean_data() function
+    database_filename: str file path into which the clean data will be saved
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    db_name = database_filename.split("/")[-1] 
+    table_name = db_name.split(".")[0]
+    df.to_sql(table_name, engine, index=False, if_exists = 'replace')
+    
 
 
 def main():
--- Disaster_response_pipeline/models/train_classifier.py
+++ Disaster_response_pipeline/models/train_classifier.py
@@ -1,24 +1,77 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger','stopwords'])
+
+import pandas as pd
+import re
+from sqlalchemy import create_engine
+import pickle 
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.externals import joblib
 
 
 def load_data(database_filepath):
-    pass
+    """
+    Args:
+
+    Returns
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table("DisasterResponse" , con = engine)
+    X,Y = df['message'], df.iloc[:,4:]
+    Y['related']=Y['related'].map(lambda x: 1 if x == 2 else x)
+    category_names = Y.columns
+    return X, Y, category_names 
 
 
 def tokenize(text):
-    pass
+    text = re.sub(r"[^a-zA-z0-9]"," ", text)
+    clean_tokens = []
+    
+    tokens = word_tokenize(re.sub(r"[^a-zA-Z0-9]"," ", text))
+    tokens = [w for w in tokens if w not in stopwords.words("english")]
+    lemmatizer = WordNetLemmatizer()
+        
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(RandomForestClassifier(class_weight='balanced')))
+    ])
 
+    parameters = {'clf__estimator__n_estimators': [50, 100],
+                  'clf__estimator__min_samples_split': [2, 3, 4],
+                  }
+
+    model = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return model
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    for col in range(len(Y_test.columns)):
+            print("Category:", Y_test.columns[col], classification_report(Y_test.values[:,col], y_pred[:,col] ))
+
 
 
 def save_model(model, model_filepath):
-    pass
+    # save model to pickle file
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
@@ -32,7 +85,7 @@ def main():
         model = build_model()
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        model.fit(X_train[0:500], Y_train[0:500])
         
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
